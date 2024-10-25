70a4088cf1d3367489a8b3b7ca4be8a3bc280361
charroux <benoit.charroux@gmail.com>
2024-10-25 09:41:32 +0200

    first commit

---
+++ src/main/java/com/example/miageGR2/MiageGr2Application.java
@@ -0,0 +1,13 @@
+package com.example.miageGR2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiageGr2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiageGr2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/miageGR2/MiageGr2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.miageGR2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiageGr2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
